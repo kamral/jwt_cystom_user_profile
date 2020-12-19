5590ae2d875e0f4a8beb9a0e313974e1286e123a
kamral <kamral010101@gmail.com>
2020-12-19 15:03:54 +0300

    add comment in serializer

--- api/views.py
+++ api/views.py
@@ -6,14 +6,32 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from .serializers import UserRegistrationSerializer
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import update_last_login
+from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
+from user.models import User
+
+JWT_PAYLOAD_HANDLER=api_settings.JWT_PAYLOAD_HANDLER
+JWT_PAYLOAD_HANDLER=api_settings.JWT_PAYLOAD_HANDLER
+
+# создание представления путем расширения CreateAPIView.
+
 
 
 class UserRegistrationView(CreateAPIView):
+    # Serializer_class сообщает, какой сериализатор использовать,
     serializer_class = UserRegistrationSerializer
+    # а permission_classes определяет, кто может получить доступ к API.
     permission_classes = (AllowAny,)
 
     def post(self, request):
+        # Разрешить любому пользователю
+        # (аутентифицированному или нет) подключаться к этой конечной точке.
+        # восстановить эти собственные типы данных в словарь проверенных данных.
         serializer = self.serializer_class(data=request.data)
+        # проверяет, соответствуют ли данные полям сериализатора,
+        # в противном случае выдается исключение.
         serializer.is_valid(raise_exception=True)
         serializer.save()
         status_code = status.HTTP_201_CREATED
--- jwt_project/settings.py
+++ jwt_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+# чтобы высчитывать разницу во времени
+from datetime import  timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -94,14 +96,57 @@ AUTH_USER_MODEL='user.User'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 REST_FRAMEWORK={
-
+# разрешение на уровне проекта
     'DEFAULT_PERMISSION_CLASSES':[
+# разрешение для авторизованных
         'rest_framework.permissions.IsAuthenticated',
+# разрешение для админа
         'rest_framework.permissions.IsAdminUser',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES':(
+# для выдачи json web token
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     )
+}
+
+# для определения настройки JWT
+# JSONWebTojenAuthetication
+JWT_AUTH={
+# JWT_ENCODE_HANDLER: полезная нагрузка, которую мы хотим кодировать.
+
+    'JWT_ENCODE_HANDLER':
+    'rest_framework_jwt.utils_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_payload_handler',
+# JWT_PAYLOAD_HANDLER: данные, которые мы хотим передать как полезную нагрузку.
+    'JWT_PAYLOAD_HANDLER':
+    'rest_framework.utils.jwt_payload_handler',
+
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+    'rest_framework_jwt.utils_get_user_id_from_payload_handler',
+# JWT_SECRET_KEY: секретный ключ для кодирования / декодирования токена
+    'JWT_SECRET_KEY':'SECRET_KEY',
+    'JWT_GET_USER_KEY':None,
+    'JWT_PUBLIC_KEY':None,
+    'JWT_PRIVATE_KEY':None,
+# JWT_ALGORITHM: какой алгоритм использовать
+    'JWT_ALGORITM':'HS256',
+# JWT_VERIFY: если мы хотим проверить токен, всегда должно быть True
+    'JWT_VERIFY':True,
+# JWT_VERIFY_EXPIRATION: если срок действия токена истек.
+    'JWT_VERIFY_EXPIRATION':True,
+    'JWT_AUDIENCE':True,
+    'JWT_ISSUER':None,
+    'JWT_ALLOW_REFRESH':False,
+# JWT_EXPIRATION_DELTA: время истечения срока действия токена, можно в минутах, часах, днях
+    'JWT_REFRESH_EXPIRATION_DELTA':timedelta(days=30),
+# JWT_AUTH_HEADER_PREFIX: префикс, который будет использоваться с токеном,
+    # как правило, мы предпочитаем Bearer
+    'JWT_AUTH_HEADER_PREFIX':'Bearer',
+    'JWT_AUTH_COOKIE':None
+
+
 }
 
 AUTH_PASSWORD_VALIDATORS = [
