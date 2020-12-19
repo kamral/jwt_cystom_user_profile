0ca5278454179ea1f77ad5d9f123c7bb0089436b
kamral <kamral010101@gmail.com>
2020-12-19 16:51:00 +0300

    add loginapiview

--- api/views.py
+++ api/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+# выдает статус кода()
 from rest_framework import status
+# создание аpi
 from rest_framework.generics import CreateAPIView
+# отправляет ответ
 from rest_framework.response import Response
+# разрешение всем
 from rest_framework.permissions import AllowAny
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
 
+
+from rest_framework.generics import RetrieveAPIView
 
 # создание представления путем расширения CreateAPIView.
 class UserRegistrationView(CreateAPIView):
@@ -33,4 +39,37 @@ class UserRegistrationView(CreateAPIView):
         }
 
         return Response(response, status=status_code)
+
+
+#
+# RetrieveAPIView
+# Используется для создания неизменяемых конечных точек для экземпляра одной модели.
+# Предоставляет: обработчик метода get.
+# Расширяет: GenericAPIView, RetrieveModelMixin
+
+class UserLoginView(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserRegistrationSerializer
+
+    def post(self,request):
+        # Разрешить любому пользователю
+        # (аутентифицированному или нет) подключаться к этой конечной точке.
+        # восстановить эти собственные типы данных в словарь проверенных данных.
+        serializer=self.serializer_class(data=request.data)
+        # проверяет, соответствуют ли данные полям сериализатора,
+        # в противном случае выдается исключение.
+        serializer.is_valid(raise_exception=True)
+        # в случае успешной авторизации
+        response={
+            'success':'True',
+            'status_code':status.HTTP_200_OK,
+            'message':'Ваш логин успешно зарегистрирован',
+            # формируем токен
+            'token':serializer.data['token']
+        }
+        status_code=status.HTTP_200_OK
+        # возвращаем ответ
+        return Response(response, status=status_code)
+
+
 
